77985d600cc2a14e3dd751062ad1859a0c198ccf
tanvi.shetty <tanvi.shetty@forcepoint.com>
2023-03-29 10:30:49 +0530

    Created folder structure

---
+++ movie-finder-website/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Component/Navbar/horizontalNav';
+function App() {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+export default App;
